155897ae0d22a6b6e839e534ea3ba9291110d67f
mkrib <mkribben@mac.com>
2023-12-21 16:29:05 +0100

    loops.js opdrachten gedaan behalve de bonus

--- loops.js
+++ loops.js
@@ -11,9 +11,9 @@
 // Dit heb je nog niet geleerd, maar bekijk hiervoor dit MDN-artikel eens: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
 // ==========================================
 
-
-
-
+for (let i = 0; i < 5; i++) {
+    console.log(`*`.repeat(i+1));
+}
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de laatste (vijfde) loop het woord "klaar!"
@@ -25,9 +25,13 @@
 // klaar!
 // ==========================================
 
-
-
-
+for (let i = 0; i < 5; i++) {
+    if (i<4) {
+        console.log("loop...");
+    } else {
+        console.log("klaar!");
+    }
+}
 
 // ==========================================
 // Opdracht 3: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
@@ -45,9 +49,15 @@
 // >> 9
 // ==========================================
 
-
-
-
+for (let i = 0; i < 10; i++) {
+    if(i < 3) {
+        console.log(i);
+    } else if (i < 6) {
+        console.log("> " + i);
+    } else {
+        console.log(">> " + i);
+    }
+}
 
 // ==========================================
 // Opdracht 4 (BONUS): schrijf een for-loop die van 0 tot 100 loopt en de getallen print.
